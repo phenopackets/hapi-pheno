0ae52756c5e3963ca42ef61f2fce0a6c9f49c20f
Peter Robinson <peter.robinson@jax.org>
2022-02-04 08:38:58 -0500

    updating resource annotation

--- src/main/java/org/monarchinitiative/hapiphenoclient/phenopacket/Measurement.java
+++ src/main/java/org/monarchinitiative/hapiphenoclient/phenopacket/Measurement.java
@@ -3,8 +3,8 @@ package org.monarchinitiative.hapiphenoclient.phenopacket;
 import ca.uhn.fhir.model.api.annotation.ResourceDef;
 import org.hl7.fhir.r4.model.Observation;
 
-@ResourceDef(name="Measurement",
-        profile="https://github.com/phenopackets/core-ig/StructureDefinition/Measurement")
+@ResourceDef(
+        profile="https://github.com/phenopackets/core-ig/fhir/StructureDefinition/Measurement")
 public class Measurement extends Observation {
 
 
